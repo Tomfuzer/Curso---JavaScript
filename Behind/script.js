5484546d9cd90412cae5eac714c8e2bde6eecb2f
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-01-17 15:24:45 -0300

    Praticando Hoisting

--- Behind/script.js
+++ Behind/script.js
@@ -1,7 +1,7 @@
 'use strict';
 
 //Aula 93 - Scoping
-
+/*
 function calcAge(birthYear) {
   const age = 2023 - birthYear;
   function printAge() {
@@ -34,3 +34,49 @@ const firstName = 'Tom';
 calcAge(1995);
 // console.log(age);
 // printAge();
+*/
+//Aula 95 - Hosting
+
+//Variaveis
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+// var me = 'Tom';
+// let job = 'Gamer';
+// const year = 1995;
+
+//Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+var addExpr = function (a, b) {
+  return a + b;
+};
+
+var addArrow = (a, b) => a + b;
+
+//Example
+//Bug por conta do Hosting, numProducts inicializou com undefined!!!
+
+console.log(numProducts);
+if (!numProducts) deleteShopppingCart();
+
+var numProducts = 10;
+
+function deleteShopppingCart() {
+  console.log('All products deleted');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
