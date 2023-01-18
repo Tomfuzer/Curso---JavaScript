f4d2ef15bab2112299e735466c91a6775e283cee
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-01-18 15:57:59 -0300

    Praticando Rest Pattern and Parameters

--- Data Structures/script.js
+++ Data Structures/script.js
@@ -33,6 +33,11 @@ const restaurant = {
     );
   },
 
+  orderPizza: function (mainIngridient, ...otherIngridients) {
+    console.log(mainIngridient);
+    console.log(otherIngridients);
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -48,6 +53,49 @@ const restaurant = {
     },
   },
 };
+
+/*
+// Aula 106 - Rest Pattern and Parameters
+
+//1) Destructuring
+
+//SPREAD cuz on right side of the =
+const arr = [1, 2, ...[3, 4]];
+
+// REST, cuz on left side of the =
+const [a, b, ...others] = [1, 2, 3, 4, 5];
+// console.log(a, b, others);
+
+const [pizza, , risotto, ...otherFood] = [
+  ...restaurant.mainMenu,
+  ...restaurant.starterMenu,
+];
+// console.log(pizza, risotto, otherFood);
+
+//Objects
+const { sat, ...weekdays } = restaurant.openingHours;
+// console.log(weekdays);
+
+//2) Functions
+const add = function (...numbers) {
+  // console.log(numbers);
+  //o parametro ...numbers(REST) pegou os valores indiviaduais passados como parametros e concatenou todos em um unico vetor, dessa forma podemos usar as propriedades numbers.lenght e sum += numbers[i] para fazer a soma total de todos os valores contidos
+  let sum = 0;
+  for (let i = 0; i < numbers.length; i++) sum += numbers[i];
+  console.log(sum);
+};
+
+add(2, 3);
+add(5, 3, 7, 2);
+add(8, 2, 5, 3, 2, 1, 4);
+
+const x = [23, 5, 7];
+add(...x);
+
+restaurant.orderPizza('Calabresa', 'Cebola', 'Cogumelos', 'Rucula');
+restaurant.orderPizza('Calabresa');
+
+*/
 
 /*
 // Aula 105 - Spread Operator (...)
