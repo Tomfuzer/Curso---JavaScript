ff7eebeb31399f67a04585ccce7abb58bb04c949
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-01-18 16:18:11 -0300

    Aprendendo sobre Nullish Coalescing Operator

--- Data Structures/script.js
+++ Data Structures/script.js
@@ -57,6 +57,14 @@ const restaurant = {
 /**/
 // Aula 108 - Nullish Coalescing Operator
 
+restaurant.numGuests = 0;
+const guests = restaurant.numGuests || 10;
+console.log(guests);
+
+// Nullish: null and undefined (NOT 0 or '')
+const guestCorrect = restaurant.numGuests ?? 10;
+console.log(guestCorrect);
+
 /*
 // Aula 107 - && and ||
 console.log('------------OR------------');
