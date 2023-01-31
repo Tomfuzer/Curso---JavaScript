d6599c8f10bcf06a7ef788d145771d6b79807368
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-01-31 16:24:52 -0300

    Desafio #2 Closure

--- Functions/starter/script.js
+++ Functions/starter/script.js
@@ -347,3 +347,21 @@ const boardPassengers = function (n, wait) {
 const perGroup = 1000;
 boardPassengers(180, 3);
 */
+
+// Aula 139
+
+/*
+//#2 Challenge
+
+let blue;
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+  blue = function () {
+    header.style.color = 'blue';
+  };
+})();
+
+document.querySelector('body').addEventListener('click', blue);
+*/
