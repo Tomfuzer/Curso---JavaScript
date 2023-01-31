bf8d0b7938257ebb0f0efce17ff0757eda6eec62
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-01-31 15:58:57 -0300

    Praticando os conceitos de closures

--- Functions/starter/script.js
+++ Functions/starter/script.js
@@ -308,3 +308,42 @@ booker();
 
 console.dir(booker);
 */
+
+// Aula 138
+/*
+let f;
+const g = function () {
+  const a = 23;
+  f = function () {
+    console.log(a * 2);
+  };
+};
+
+const h = function () {
+  const b = 777;
+  f = function () {
+    console.log(b * 2);
+  };
+};
+
+g();
+f();
+//F foi redefinido, quando isso acontece ela 'nasce' novamente, então fica atrelada a um novo escopo, perdendo por exemplo a conexão com as variáveis presente na função g
+h();
+f();
+console.dir(f);
+
+// Example 2
+const boardPassengers = function (n, wait) {
+  const perGroup = n / 3;
+
+  setTimeout(function () {
+    console.log(`We are now boarding all ${n} passengers`);
+    console.log(`There are three groups, each with ${perGroup} passengers`);
+  }, wait * 1000);
+  console.log(`Will start boarding in ${wait} seconds`);
+};
+
+const perGroup = 1000;
+boardPassengers(180, 3);
+*/
