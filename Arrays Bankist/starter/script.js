e3e54c4b017e07d80a06efdb75214f4141e128b1
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-02-01 14:47:42 -0300

    Manipulação de DOM do projeto Bank

--- Arrays Bankist/starter/script.js
+++ Arrays Bankist/starter/script.js
@@ -61,6 +61,24 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+//Projeto Banco
+
+const displayMoviments = function (mov) {
+  containerMovements.innerHTML = '';
+
+  mov.forEach(function (mov, i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `<div class="movements__row">
+    <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+        <div class="movements__value">${mov}</div>
+  </div>;`;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html); // parametros de insertAdjacentHTML('aonde colocar', 'o que colocar')
+  });
+};
+displayMoviments(account1.movements); // chamou a função displayMoviments que fez a inserção no HTML com as informações específicas da account1
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
