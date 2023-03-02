010f952c23486bea5c0d34e874576c9c9b6b979f
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-03-02 13:52:41 -0300

    Aula 198 - Reveal elements on Scroll c/ Inters. Obs. API

--- Adv Dom/script.js
+++ Adv Dom/script.js
@@ -343,7 +343,7 @@ const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
-  console.log(entry);
+  // console.log(entry);
   if (!entry.isIntersecting) nav.classList.add('sticky');
   else nav.classList.remove('sticky');
 };
@@ -354,3 +354,25 @@ const headerObserver = new IntersectionObserver(stickyNav, {
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+// Aula 198 - Revelar elementos on Scroll c/ Intersection API
+
+const revalSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve(entry.target);
+};
+
+const sectionObserver = new IntersectionObserver(revalSection, {
+  root: null, //when null root = viewport
+  threshold: 0.15,
+});
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
+
+//
