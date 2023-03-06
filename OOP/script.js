0579b51655e4393fb992bfc41dc2c7ec152bc15b
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-03-06 14:20:57 -0300

    OOP em JavaScript

---
+++ OOP/script.js
@@ -0,0 +1,30 @@
+'use strict';
+
+// Aula 207 - OOP in JavaScript
+
+// Aula 208 - Constructor Functions and the new Operator  - Pratica
+
+// Constructor function
+const Person = function (firstName, birthYear) {
+  //Propriedades instanciadas
+  this.firstName = firstName;
+  this.birthYear = birthYear;
+
+  //Não criar metódos dentro de construtores
+  //   this.calcAge = function(){
+  //     console.log(2023 - this.birthYear);
+  //   }
+};
+
+const tom = new Person('Tom', 1995);
+console.log(tom);
+
+// 1. Novo objeto vazio é criado {}
+// 2. Função é criada, this = {}
+// 3. {} linkado ao prototype
+// 4. A função retorna automaticamente o objeto vazio {}
+
+const rafaela = new Person('Rafaela', 1997);
+console.log(rafaela);
+
+console.log(tom instanceof Person);
