9f1a6aec225c2449eaf6cf482122a2c4dac95d08
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-03-13 15:31:35 -0300

    att.

--- Mapty/script.js
+++ Mapty/script.js
@@ -20,6 +20,21 @@ if (navigator.geolocation) {
       const { latitude } = position.coords;
       const { longitude } = position.coords;
       console.log(`https://www.google.com.br/maps/@${latitude},${longitude}`);
+
+      const coords = [latitude, longitude];
+
+      const map = L.map('map').setView(coords, 13);
+
+      // Aula 233 - Displaying a Map Using Leaflet Library
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker(coords)
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
     },
     function () {
       alert('Não foi possível acessar sua localização!');
