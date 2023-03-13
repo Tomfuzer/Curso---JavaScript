406412daba25bef767675e77c67193a908fb3f3c
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-03-13 16:23:49 -0300

    Rendering Workout Input Form

--- Mapty/script.js
+++ Mapty/script.js
@@ -11,6 +11,8 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
+let map, mapEvent;
+
 // Aula 232 - Using the Geolocation API
 //função / API geolocalização
 if (navigator.geolocation) {
@@ -23,7 +25,7 @@ if (navigator.geolocation) {
 
       const coords = [latitude, longitude];
 
-      const map = L.map('map').setView(coords, 13);
+      map = L.map('map').setView(coords, 13);
       // console.log(map);
 
       // Aula 233 - Displaying a Map Using Leaflet Library
@@ -33,23 +35,10 @@ if (navigator.geolocation) {
       }).addTo(map);
 
       // Aula 234 - Displaying a Map Marker
-      map.on('click', function (mapEvent) {
-        console.log(mapEvent);
-        const { lat, lng } = mapEvent.latlng;
-
-        L.marker([lat, lng])
-          .addTo(map)
-          .bindPopup(
-            L.popup({
-              maxWidth: 250,
-              minWidth: 100,
-              autoClose: false,
-              closeOnClick: false,
-              className: 'running-popup',
-            })
-          )
-          .setPopupContent('Workout')
-          .openPopup();
+      map.on('click', function (mapE) {
+        mapEvent = mapE; //alocando mapE para uma variável global
+        form.classList.remove('hidden');
+        inputDistance.focus(); // focar no campo UX++
       });
     },
     function () {
@@ -57,3 +46,34 @@ if (navigator.geolocation) {
     }
   );
 }
+// Aula 235 - Rendering Workout Input Form
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  //Clear input fields
+  inputDistance.value =
+    inputCadence.value =
+    inputDuration.value =
+    inputElevation.value =
+      '';
+  //Mostrar marcação
+  console.log(mapEvent);
+  const { lat, lng } = mapEvent.latlng;
+  L.marker([lat, lng])
+    .addTo(map)
+    .bindPopup(
+      L.popup({
+        maxWidth: 250,
+        minWidth: 100,
+        autoClose: false,
+        closeOnClick: false,
+        className: 'running-popup',
+      })
+    )
+    .setPopupContent('Workout')
+    .openPopup();
+});
+
+inputType.addEventListener('change', function () {
+  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+});
