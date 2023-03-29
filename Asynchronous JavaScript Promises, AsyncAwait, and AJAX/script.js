e1f4f3ab5ecaa3a5014bd34a699f4e53a03149b8
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-03-29 13:25:38 -0300

    Event loop in Practice

--- Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
+++ Asynchronous JavaScript Promises, AsyncAwait, and AJAX/script.js
@@ -218,7 +218,7 @@ const getCountryData = function (country) {
 };
 
 // btn.addEventListener('click', function () {
-//   getCountryData('chile');
+//   getCountryData('italia');
 // });
 
 //Coding Challenge #1
@@ -260,7 +260,7 @@ const whereAmI = function (userCoords) {
       console.log(`You are in ${data.city}, ${data.countryName}`);
       getCountryData(data.countryName);
     })
-    .catch();
+    .catch(err => console.error(`${err.message}`));
 };
 
 const getLocal = function () {
@@ -279,10 +279,22 @@ const getLocal = function () {
   }
 };
 
-getLocal();
+// getLocal();
 // whereAmI([52.508, 13.381]);
 // whereAmI([19.037, 72.873]);
 // whereAmI([-33.933, 18.474]);
 // btn.addEventListener('click', function () {
 //   getCountryData('brazil');
 // });
+
+// Aula 258 - Event loop in Practice
+
+// console.log('Test start');
+// setTimeout(() => console.log('0 sec timer'), 0);
+// Promise.resolve('Resolved promise 1').then(res => console.log(res));
+// Promise.resolve('Resolved promise 2').then(res => {
+//   for (let i = 0; i < 10000000000; i++) {}
+//   console.log(res);
+// });
+
+// console.log('Teste end');
