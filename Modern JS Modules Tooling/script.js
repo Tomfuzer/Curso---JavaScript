03edff3a87c40c1233111efb19e072f338561b64
Hamilton Fuzer <tomfuzer@hotmail.com>
2023-04-06 14:50:44 -0300

    Module Pattern

--- Modern JS Modules Tooling/script.js
+++ Modern JS Modules Tooling/script.js
@@ -11,24 +11,24 @@
 // ShoppingCart.addToCart('pães', 3);
 // console.log(ShoppingCart.totalPrice);
 
-import add, { cart } from './shoppingCart.js';
-// add('pizzas', 3);
-// add('maças', 4);
-// add('bananas', 5);
-// console.log(cart);
+// import add, { cart } from './shoppingCart.js';
+// // add('pizzas', 3);
+// // add('maças', 4);
+// // add('bananas', 5);
+// // console.log(cart);
 
-// const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-// const data = await res.json();
-// console.log(data);
-// console.log('Alguma coisa');
+// // const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+// // const data = await res.json();
+// // console.log(data);
+// // console.log('Alguma coisa');
 
-const getLastPost = async function () {
-  const res = await fetch('https://jsonplaceholder.typicode.com/posts');
-  const data = await res.json();
-  //   console.log(data);
+// const getLastPost = async function () {
+//   const res = await fetch('https://jsonplaceholder.typicode.com/posts');
+//   const data = await res.json();
+//   //   console.log(data);
 
-  return { title: data.at(-1).title, text: data.at(-1).body };
-};
+//   return { title: data.at(-1).title, text: data.at(-1).body };
+// };
 
 // const lastPost = getLastPost();
 // console.log(lastPost);
@@ -36,5 +36,32 @@ const getLastPost = async function () {
 //Not very clean
 // lastPost.then(last => console.log(last));
 
-const lastPost2 = await getLastPost();
-console.log(lastPost2);
+// const lastPost2 = await getLastPost();
+// console.log(lastPost2);
+
+const ShoppingCart2 = (function () {
+  const cart = [];
+  const shippingCost = 10;
+  const totalPrice = 250;
+  const totalQuantity = 25;
+
+  const addToCart = function (produto, quantidade) {
+    cart.push({ produto, quantidade });
+    console.log(`${quantidade} ${produto} adicionado ao carrinho`);
+  };
+
+  const orderStock = function (produto, quantidade) {
+    console.log(`${quantidade} ${produto} ordens do fornecedor`);
+  };
+
+  return {
+    addToCart,
+    cart,
+    totalPrice,
+    totalQuantity,
+  };
+})();
+
+ShoppingCart2.addToCart('apple', 4);
+ShoppingCart2.addToCart('pizza', 2);
+console.log(ShoppingCart2);
